a1c7baed2c5f183e05c10752f86ba19a28de1ccf
Roman <romandi@gmail.com>
2018-05-02 19:18:52 -0400

    Mona v218, libsrt v1.2.3, MonaSRT v0.3

--- buildsrt.sh
+++ buildsrt.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-[ -d srt-1.2.2 ] || {
-	wget https://github.com/Haivision/srt/archive/v1.2.2.tar.gz || { 
+SRTVER="1.2.3"
+[ -d srt-$SRTVER ] || {
+	wget https://github.com/Haivision/srt/archive/v$SRTVER.tar.gz || { 
 		echo "can't download SRT."; 
 		exit 1;
 	}
-	tar -xvf v1.2.2.tar.gz
+	tar -xvf v$SRTVER.tar.gz
 }
 
-cd srt-1.2.2 && {
+cd srt-$SRTVER && {
 	./configure --prefix=../srt
 	make && make install
 } || {
--- include/MonaSRTVersion.h
+++ include/MonaSRTVersion.h
@@ -1,7 +1,7 @@
 #include "Mona/Mona.h"
 #include "Version.h"
 
-#define MONASRT_VERSION 1.0
+#define MONASRT_VERSION 0.3
 
 #define MONASRT_VERSION_STRING \
 	"1." STRINGIZE(MONA_VERSION) \
