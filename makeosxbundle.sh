07e355bafe4a470ba09ead155576b05c2e730dcd
Roman <romandi@gmail.com>
2018-05-15 00:40:02 -0400

    Attempt to fix artifact build dir path 8

--- makeosxbundle.sh
+++ makeosxbundle.sh
@@ -17,4 +17,5 @@ mv RTMPtoSRT $INSTALLDIR
 #not sure why this is needed but it is. probably a bug in dylibbundler.
 [ -f $INSTALLDIR/libs/libssl.1.0.0.dylib ] && {
 	install_name_tool -change /usr/local/Cellar/openssl/1.0.2o_1/lib/libcrypto.1.0.0.dylib @executable_path/libs/libcrypto.1.0.0.dylib $INSTALLDIR/libs/libssl.1.0.0.dylib
+	install_name_tool -change /usr/local/Cellar/openssl/1.0.2m/lib/libcrypto.1.0.0.dylib @executable_path/libs/libcrypto.1.0.0.dylib $INSTALLDIR/libs/libssl.1.0.0.dylib
 }
